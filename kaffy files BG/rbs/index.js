76bb611d2fd7b8470b7830861cbac5a12070fe9d
adeshewa12345 <133238028+adeshewa12345@users.noreply.github.com>
2023-05-11 07:15:36 -0700

    Add files via upload

---
+++ kaffy files BG/rbs/index.js
@@ -0,0 +1,69 @@
+//javascript string method "length"
+
+let names = "javascript";
+let output = names.length
+console.log(names.length);
+
+
+// javascript slice method 
+ console.log(names.slice(0,4))
+ console.log( names.slice(4,10))
+
+ //javascript string method substring
+   console.log(names.substr(-1,4));
+   let numeri = "7"
+   //javascript replace method 
+   console.log(names.replace("a","o"))
+    
+  console.log(names.replaceAll("a","o"))
+  let anarray = [1,2,3,"7",false,]
+
+  let name2 = "lang"
+  let name3= names.concat(name2)
+   console.log(name3)
+   console.log(names.charAt(6));
+    let sentence = "javascript is a scripting language";
+    console.log(sentence.indexOf("is"));
+    console.log(sentence.indexOf("scripting"));
+    console.log(sentence.lastIndexOf("language"));
+    console.log(sentence.match("lang"));
+  let num = 23
+  let letter ="x"
+  let letter2 ="y"
+  let result = "56"
+  console.log(result)
+
+
+    // console.log(names.slice(senator,abiru))
+    // console.log(names.slice(innovation,labs))
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
